e806c11ff6c43b016670ddee99184479a0578f54
SolomkaMykola <solomkamykola@gmail.com>
2024-03-05 23:39:46 +0200

    Optimized tests with database

---
+++ main_ltew_132012_83j7.js
@@ -0,0 +1,15 @@
+if (getRandomBoolean()) { console.log("True"); } else { console.log("False"); }
+orange
+
+
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
+false / false
+const findMissingNumber = array => { const n = array.length + 1; const expectedSum = (n * (n + 1)) / 2; const actualSum = array.reduce((sum, num) => sum + num, 0); return expectedSum - actualSum; };
+const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
+const memoize = fn => { const cache = new Map(); return (...args) => { const key = JSON.stringify(args); return cache.has(key) ? cache.get(key) : (cache.set(key, fn(...args)), cache.get(key)); }; };
+const isEven = num => num % 2 === 0;
+orange / true
+const sumOfPrimes = limit => { const isPrime = num => { for (let i = 2; i < num; i++) if (num % i === 0) return false; return num > 1; }; return Array.from({ length: limit }, (_, index) => index).filter(isPrime).reduce((sum, prime) => sum + prime, 0); };
+grape
+
+let array = getRandomArray(); array.forEach(item => console.log(item));
